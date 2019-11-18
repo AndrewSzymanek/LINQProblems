2099936ace538408092f3a486c3a66e0b269993d
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-18 12:00:57 -0600

    have third question logic complete except for taking an average of all of the class averages

---
+++ LINQProblems/ClassGradeAverage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class ClassGradeAverage
+    {
+        List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+        
+        public static List<double> AverageGrades(List<string> items)
+        {
+            List<double> averages = new List<double>();
+            foreach (string item in items)
+            {    
+                string[] grades = SplitOffLowestGrade(item);
+                double[] gradesAsDoubles = MakeGradesArrayIntoDoubles(grades);
+                double average = gradesAsDoubles.Average();
+                averages.Add(average);
+            }
+            return averages;
+        }
+        public static string[] SplitOffLowestGrade(string item)
+        {
+            var output = item.Split(',').OrderBy(i => int.Parse(i)).ToList();
+            output.RemoveAt(0);
+            return output.ToArray();
+        }
+        public static double[] MakeGradesArrayIntoDoubles(string[] item)
+        {
+            double[] doubleArray = new double[item.Length];
+            for(int i = 0; i < item.Length; i++)
+            {
+                doubleArray[i] = double.Parse(item[i]);
+            }
+            return doubleArray;
+        }
+       
+
+    }
+}
