159eb25704442bfe745e26ab95e8716b079edda9
Andrew Szymanek <andrewszymanek@Andrews-MacBook-Pro.local>
2019-11-18 09:26:57 -0600

    first two problems complete out of four

---
+++ LINQProblems/StringClassWithoutDuplicates.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class StringClassWithoutDuplicates
+    {
+        List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+        public static List<string> ListWithoutDuplicates(List<string> items)
+        {
+            return items.Distinct().ToList();
+        }
+
+    }
+}
---
+++ LINQProblems/SubStringFinder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQProblems
+{
+    class SubStringFinder
+    {
+        List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics"};
+
+        public static List<string> GetThWords(List<string> input)
+        {
+            return input.Where(w => w.ToLower().Contains("th")).ToList();
+        }
+       
+    }
+}
